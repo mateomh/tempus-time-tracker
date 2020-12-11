785df8fa25fa0a87e5812bf50a7d2855ae46b9ed
mateomh <mmojicah.84@gmail.com>
2020-12-11 11:46:24 -0500

    Add configuration to the store to see it in the redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -5,7 +6,10 @@ import { createStore } from 'redux';
 import App from './components/app';
 import rootReducer from './reducers/root';
 
-const myStore = createStore(rootReducer);
+const myStore = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(
   <Provider store={myStore}>
