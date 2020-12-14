d912700d77c9370f1fc81521cec4ee43c2445199
mateomh <mmojicah.84@gmail.com>
2020-12-14 12:10:49 -0500

    Make the totaldisplay component show the name of the category

--- src/components/totaldisplay.js
+++ src/components/totaldisplay.js
@@ -5,7 +5,7 @@ import DisplayStyles from '../assets/stylesheets/totaldisplay.module.css';
 
 const TotalDisplay = () => {
   const store = useStore();
-  const { sessionUser } = store.getState();
+  const { sessionUser, filter } = store.getState();
   const history = useHistory();
 
   if (Object.keys(sessionUser).length === 0) {
@@ -17,7 +17,9 @@ const TotalDisplay = () => {
       <p className={DisplayStyles.Text}>
         {sessionUser.name}
         &rsquo;s&nbsp;
-        total time for All categories
+        total time for&nbsp;
+        {filter.name === undefined ? 'All' : filter.name}
+        &nbsp;categories
       </p>
       <p className={DisplayStyles.Number}>1169 hours</p>
     </div>
