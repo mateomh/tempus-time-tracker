3fd95f511914b1926e7539fb0bc2c0ff8d64e90a
mateomh <mmojicah.84@gmail.com>
2020-12-16 20:55:45 -0500

    Add persistance to the sessionUser state in localstorage

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -3,8 +3,13 @@ const sessionReducer = (state = {}, action) => {
 
   switch (type) {
     case 'CREATE_SESSION':
+      localStorage.setItem(
+        'sessionUser',
+        JSON.stringify(user),
+      );
       return user;
     case 'DESTROY_SESSION':
+      localStorage.removeItem('sessionUser');
       return {};
     default:
       return state;
